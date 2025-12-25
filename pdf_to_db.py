07a07b134314c3c94c05315d4ed6fc0b7bdea8c2
MSV2000 <maximsinyakin00@gmail.com>
2025-12-25 13:40:44 +0300

    Added logic in def add_pdf_to_db

--- pdf_to_db.py
+++ pdf_to_db.py
@@ -1,6 +1,8 @@
 import os
 import fitz
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from typing import List
 
 
 class PDFVecDataBase:
@@ -11,8 +13,8 @@ class PDFVecDataBase:
         Инициализация класса
 
         Args:
-        embeddings_model: имя модели эмбеддингов
-        path_db: путь до векторной базы данных
+            embeddings_model: имя модели эмбеддингов
+            path_db: путь до векторной базы данных
         """
 
         # Инициализация модели эмбеддингов (будет на CPU)
@@ -70,7 +72,44 @@ class PDFVecDataBase:
             if doc:
                 doc.close()
 
-    def add_pdf_to_db(self, file_path: str, text_splitter, start_page: int = 2):
+    def add_pdf_to_db(self, file_path: str, collection_name: str, text_splitter: RecursiveCharacterTextSplitter = None,
+                      start_page: int = 1):
+        """
+        Извлечение текста из PDF файла и добавление в векторную базу данных
+
+        Args:
+            file_path: путь к PDF файлу
+            collection_name: название коллекции
+            text_splitter: объект для разделения текста
+            start_page: номер страницы с которой начинать извлечение (по умолчанию 1)
+
+        Returns:
+            retriever: объект для поиска по векторной базе
+
+        Raises:
+            ValueError: если файл или collection_name пустые
+        """
+        if collection_name == "":
+            raise ValueError(f"Название коллекции не должно быть пустым")
+
+        text = self.extract_text_from_pdf(file_path=file_path, start_page=start_page)
+
+        if not text or text.strip() == "":
+            raise ValueError(f"PDF файл {file_path} не содержит текст")
+
+        if text_splitter:
+            split_text = text_splitter.split_text(text=text)
+        else:
+            split_text = [text]
+
+        # Добавляем в базу данных
+        return self.add_texts_to_db(
+            text=split_text,
+            collection_name=collection_name,
+            overwrite=False
+        )
+
+    def add_texts_to_db(self, text: List[str], collection_name: str, overwrite: bool = False):
         pass
 
 
@@ -78,4 +117,7 @@ if __name__ == "__main__":
     pdf_db = PDFVecDataBase(embeddings_model="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2",
                             path_db="chroma_db")
 
-    print(pdf_db.extract_text_from_pdf(file_path="example.pdf", start_page=2))
+    splitter = RecursiveCharacterTextSplitter(chunk_size=3000, chunk_overlap=1500,
+                                              separators=["\n\n", "\n", ",", " ", ""])
+
+    pdf_db.add_pdf_to_db(file_path="example.pdf", collection_name="", text_splitter=splitter, start_page=2)
