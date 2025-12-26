d76b8e27025959b73b94eb9fd584070d2a2daa96
MSV2000 <maximsinyakin00@gmail.com>
2025-12-26 10:29:16 +0300

    Added logic for saving a pdf file to the add_pdf_to_db function

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import aiofiles
 import uvicorn
 from dataclasses import dataclass
-from fastapi import FastAPI, HTTPException, UploadFile, File
+from fastapi import FastAPI, HTTPException, UploadFile, File, Form
 from pathlib import Path
 from typing import List
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from pdf_to_db import PDFVecDataBase
-
 
 pdf_db = PDFVecDataBase(embeddings_model="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2",
                         path_db="chroma_db")
@@ -21,14 +20,6 @@ app = FastAPI()
 
 
 @dataclass
-class UploadRequest:
-    collection_name: str
-    start_page: int
-    overwrite: bool
-    files: List[UploadFile] = File(...)
-
-
-@dataclass
 class UserRequest:
     question: str
 
@@ -39,53 +30,53 @@ class UserResponse:
 
 
 @app.post("/add_pdf_to_db")
-async def add_pdf_to_db(collection_name: str, start_page: int, overwrite: bool, files: List[UploadFile] = File(...)):
-    # files = data.files
-    # collection_name = data.collection_name
-    # start_page = data.start_page
-    # overwrite = data.overwrite
+async def add_pdf_to_db(collection_name: str = Form(..., description="Название коллекции"),
+                        start_page: int = Form(1, description="Страница, с которой начать обработку (по умолчанию 1)"),
+                        overwrite: bool = Form(False, description="Перезаписать коллекцию если существует (по умолчанию False)"),
+                        file: UploadFile = File(..., description="PDF файл для загрузки")):
 
-    if not files:
-        raise HTTPException(status_code=400, detail="Не переданы файлы")
+    if not file:
+        raise HTTPException(status_code=400, detail="Файл не передан")
 
-    saved_files = []
-    invalid_files = []
+    filename = file.filename
 
-    for file in files:
-        safe_filename = file.filename
+    # Проверка расширения файла (опционально)
+    if not filename.lower().endswith('.pdf'):
+        raise HTTPException(status_code=400, detail=f"Файл {filename} не имеет расширения .pdf")
 
-        # Проверка расширения файла (опционально)
-        if not safe_filename.lower().endswith('.pdf'):
-            invalid_files.append(safe_filename)
-            continue
+    file_path = UPLOAD_DIR / filename
 
-        file_path = UPLOAD_DIR / safe_filename
+    # Асинхронное сохранение файла
+    try:
+        async with aiofiles.open(file_path, 'wb') as buffer:
+            content = await file.read()
+            await buffer.write(content)
 
-        # Асинхронное сохранение файла
-        try:
-            async with aiofiles.open(file_path, 'wb') as buffer:
-                content = await file.read()
-                await buffer.write(content)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Файл {filename} не загружен: {e}")
 
-            saved_files.append(safe_filename)
-        except Exception as e:
-            invalid_files.append(f"{safe_filename}: {str(e)}")
+    try:
+        pdf_db.add_pdf_to_db(file_path=str(file_path), collection_name=collection_name, text_splitter=splitter,
+                             start_page=start_page, overwrite=overwrite)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Ошибка на этапе извлечения текста из PDF файла и добавления в "
+                                                    f"векторную базу данных: {e}")
 
-    if not saved_files:
-        raise HTTPException(status_code=400, detail="Нет допустимых файлов для загрузки")
-
-    for safe_file in saved_files:
-        file_path = str(UPLOAD_DIR / safe_file)
-
-        pdf_db.add_pdf_to_db(file_path=file_path, collection_name=collection_name, text_splitter=splitter,
-                             start_page=start_page, overwrite=False)
-
+    if overwrite:
+        return {
+            "message": f"Коллекция {collection_name} перезаписана. Файл {filename} загружен в коллекцию",
+            "collection_name": collection_name,
+            "filename": filename,
+            "status": "success",
+            "action": "overwritten"
+        }
     return {
-        "message": "Обработка файлов завершена",
-        "saved_files": saved_files,
-        "invalid_files": invalid_files,
+        "message": f"Файл {filename} загружен в коллекцию {collection_name}",
+        "collection_name": collection_name,
+        "filename": filename,
+        "status": "success",
+        "action": "added"
     }
-
 
 @app.post("/question")
 async def answers_questions(data: UserRequest) -> UserResponse:
